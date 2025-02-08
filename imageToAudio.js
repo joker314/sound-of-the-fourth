49002992b09b06c88e4ec7c0a809ce19d51be139
x <>
2025-02-08 21:12:21 +0000

    smooth boundary spheres

--- imageToAudio.js
+++ imageToAudio.js
@@ -88,17 +88,27 @@ for (const point of points) {
 srcCtx.stroke();
 
 // let circle = {x: 0.3, y: 0.5, r: 0.3};
-let circle = {center: [0.3, 0.5, 0.5], r: 0.3};
+const circles = [
+    {center: [0.3, 0.5, 0.5], r: 0.3},
+    {center: [0.1, 0.1, 0.2], r: 0.3},
+]
 
 const getAmplitude = point => {
     // if ((point[0] - circle.x)**2 + (point[1] - circle.y)**2 < circle.r**2) {
     //     return 1
     // }
 
-    const distSquared = point.map((cord, i) => (cord - circle.center[i])**2).reduce((s,c) => s+c, 0);
-    if (distSquared < circle.r**2) {
-        return 1;
+    let s = 0;
+
+    for(const circle of circles) {
+        const distSquared = point.map((cord, i) => (cord - circle.center[i])**2).reduce((s,c) => s+c, 0);
+        // if (distSquared < circle.r**2) {
+        const v = 1/(1 + distSquared / circle.r**2);
+        s += v**8;
+        // }
     }
+
+    return s;
 
 
     // if ((point[0] - 0.1)**2 + (point[1] - 0.9)**2 < 0.2**2) {
@@ -147,28 +157,29 @@ const render = () => {
     for (const point of points) {
         const amplitude = getAmplitude(point);
         if (amplitude === 0) continue;
-        srcCtx.fillStyle = `hsl(0, 100%, ${amplitude*50}%)`;
+        // srcCtx.fillStyle = `hsl(0, 100%, ${amplitude*50}%)`;
+        srcCtx.fillStyle = `hsla(0, 100%, 50%, ${amplitude})`;
         srcCtx.fillRect(point[0] * w, point[1] * h, 3, 3);
     }
 }
 
 document.body.addEventListener("keydown", e => {
     if (e.key === 'ArrowDown') {
-        circle.center[1] += 0.02;
+        circles[0].center[1] += 0.02;
     } else if (e.key === 'ArrowUp') {
-        circle.center[1] -= 0.02;
+        circles[0].center[1] -= 0.02;
     } else if (e.key === 'ArrowLeft') {
-        circle.center[0] -= 0.02;
+        circles[0].center[0] -= 0.02;
     } else if (e.key === 'ArrowRight') {
-        circle.center[0] += 0.02;
+        circles[0].center[0] += 0.02;
     } else if (e.key === '[') {
-        circle.r -= 0.02;
+        circles[0].r -= 0.02;
     } else if (e.key === ']') {
-        circle.r += 0.02;
+        circles[0].r += 0.02;
     } else if (e.key === ',') {
-        circle.center[2] -= 0.02;
+        circles[0].center[2] -= 0.02;
     } else if (e.key === '.') {
-        circle.center[2] += 0.02;
+        circles[0].center[2] += 0.02;
     }
     render()
     audioCtx.resume()
