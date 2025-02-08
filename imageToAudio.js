472070ed3e17cbc1725765b963ad289903bb38d2
x <>
2025-02-08 17:54:23 +0000

    clean up

--- imageToAudio.js
+++ imageToAudio.js
@@ -24,7 +24,6 @@ srcCanvas.height = h;
 const srcCtx = srcCanvas.getContext('2d');
 document.body.append(srcCanvas);
 
-// srcCtx.fillRect(50,100,50,50);
 
 const points = hilbertCurve(5);
 
@@ -53,28 +52,6 @@ for (const point of points) {
     srcCtx.fillRect(point[0] * w, point[1] * h,3,3);
 }
 
-
-
-
-
-// function createSuperposedSine(frequencies, duration = 2) {
-//     const gainNode = audioCtx.createGain();
-//     gainNode.gain.value = 0.1; // Adjust overall volume
-//     gainNode.connect(audioCtx.destination);
-    
-//     frequencies.forEach(freq => {
-//         const oscillator = audioCtx.createOscillator();
-//         oscillator.type = 'sine';
-//         oscillator.frequency.value = freq;
-//         oscillator.connect(gainNode);
-//         oscillator.start();
-//         oscillator.stop(audioCtx.currentTime + duration);
-//     });
-// }
-
-// // Example: Superposition of 300Hz, 400Hz, and 500Hz
-// createSuperposedSine([300, 400, 500]);
-
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 const minFreq = 100;
@@ -82,7 +59,6 @@ const maxFreq = 10000;
 
 const gainNodes = Array(points.length);
 for (let i=0; i < points.length; i++) {
-    const point = points[i];
     const gainNode = audioCtx.createGain();
     gainNode.gain.value = 0;
     gainNode.connect(audioCtx.destination);
@@ -96,18 +72,7 @@ for (let i=0; i < points.length; i++) {
     gainNodes[i] = gainNode;
 }
 
-function createSound({duration = 2} = {}) {
-    
-    // frequencies.forEach(freq => {
-    //     const oscillator = audioCtx.createOscillator();
-    //     oscillator.type = 'sine';
-    //     oscillator.frequency.value = freq;
-    //     oscillator.connect(gainNode);
-    //     oscillator.start();
-    //     oscillator.stop(audioCtx.currentTime + duration);
-    // });
-
-
+const updateSound = () => {
     for (let i=0; i < points.length; i++) {
         const point = points[i];
         const gainNode = gainNodes[i];
@@ -115,20 +80,13 @@ function createSound({duration = 2} = {}) {
     }
 }
 
-// createSound()
-
-window.addEventListener("click", () => audioCtx.resume())
-
 const render = () => {
-    const interval = 1/10;
-    createSound({duration: interval});
+    updateSound();
     srcCtx.clearRect(0, 0, w, h);
     for (const point of points) {
         srcCtx.fillStyle = `hsl(0, 100%, ${getAmplitude(point)*50}%)`;
         srcCtx.fillRect(point[0] * w, point[1] * h, 3, 3);
     }
-
-    // setTimeout(render, 1000/60);
 }
 
 document.body.addEventListener("keydown", e => {
