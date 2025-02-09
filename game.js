daef2f2345b4df85493c982679fae578fa66bcc9
x <>
2025-02-09 12:34:36 +0000

    add further away outer sample points

--- game.js
+++ game.js
@@ -550,7 +550,14 @@ const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const minFreq = 100
 const maxFreq = 3000
 
-const pointsForSoundSampling = zOrderCurveND(4, 2).map(point => new HighDimensionalVector(point));
+// const pointsForSoundSampling = zOrderCurveND(4, 2).map(point => new HighDimensionalVector(point));
+
+const dimensions = 4;
+const offset = new HighDimensionalVector(Array(dimensions).fill(1)).scale(0.5).negate();
+const pointsForSoundSampling = [
+    ...zOrderCurveND(dimensions, 2).map(point => new HighDimensionalVector(point).add(offset)),
+    ...zOrderCurveND(dimensions, 2).map(point => new HighDimensionalVector(point).add(offset).scale(3)),
+]
 const gainNodes = setUpGainNodes(audioCtx, minFreq, maxFreq, pointsForSoundSampling.length)
 
 window.addEventListener("keydown", (e) => {
@@ -562,8 +569,8 @@ window.addEventListener("keydown", (e) => {
     player.rayTrace(maze, output, gains)
 
     updateSound(gainNodes, pointsForSoundSampling, point => {
-        const offset = new HighDimensionalVector(Array(point.components.length).fill(1)).scale(0.5).negate();
-        const scaledAndOffsetPoint = point.add(offset).scale(100);
+        // const offset = new HighDimensionalVector(Array(point.components.length).fill(1)).scale(0.5).negate();
+        const scaledAndOffsetPoint = point.scale(100);
 
         const rotatedPoint = (new Matrix(player.basis)).transformHighDimensionalVector(scaledAndOffsetPoint);
 
